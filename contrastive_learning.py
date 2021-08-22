3872385cdf451cf0de1d51740ac94b8c9687ad47
emil <e.pileckis@yandex.ru>
2021-08-22 17:52:59 +0300

    Forgot this is number of classes, and should be 10

--- contrastive_learning.py
+++ contrastive_learning.py
@@ -29,7 +29,7 @@ class Net(gluon.Block):
             self.conv2 = nn.Conv2D(8, kernel_size=(5, 5))
             self.pool2 = nn.MaxPool2D(pool_size=(2, 2), strides=(2, 2))
             self.fc1 = nn.Dense(64)
-            self.fc2 = nn.Dense(8)
+            self.fc2 = nn.Dense(10)  # this is equal to number of classes
 
     def forward(self, x):
         x = self.pool1(F.tanh(self.conv1(x)))
